14f13dce0899b064b602f5a8335d1c41c7333dc1
tonyshen1999 <tony.tr.shen@gmail.com>
2023-01-14 14:30:57 -0800

    file import all sheets from xlsx

--- src/app/pages/import/import.component.ts
+++ src/app/pages/import/import.component.ts
@@ -7,20 +7,30 @@ import { read, readFile, writeFileXLSX} from "xlsx";
   styleUrls: ['./import.component.scss']
 })
 export class ImportComponent implements OnInit {
-  private sheetHash:Map<string,[]> = new Map<string,[]>();
-  private sheetNames:string[] = ["None"];
+  sheetHash:Map<string,[]> = new Map<string,[]>();
+  sheetNames:string[] = ["None"];
+  sheetCount:Map<string,number> = new Map<string,number>();
   disableButton:boolean = true;
+  showTable:boolean = false;
   constructor() { }
 
   DataFromEventEmitter(event){
 
     this.sheetHash = event;
     this.sheetNames = Array.from(this.sheetHash.keys());
+    for(let i=0;i<this.sheetNames.length;i++){
+      let count:number = 0;
+      if(this.sheetHash.get(this.sheetNames[i])!=null){
+        count = this.sheetHash.get(this.sheetNames[i]).length;
+      }
+      this.sheetCount.set(this.sheetNames[i],count)
+    }
     // console.log(event);
     let sheet_json = JSON.stringify(this.sheetHash.get(this.sheetNames[4]))
     // console.log(sheet_json)
     if (this.sheetNames.length >0){
       this.disableButton = false;
+      this.showTable = true;
     }
   }
   ngOnInit(): void {
