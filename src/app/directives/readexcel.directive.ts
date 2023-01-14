c015f3daeea7ff7cbe8cbbb1e5420e02676bea69
tonyshen1999 <tony.tr.shen@gmail.com>
2023-01-14 13:00:40 -0800

    file import all sheets from xlsx

--- src/app/directives/readexcel.directive.ts
+++ src/app/directives/readexcel.directive.ts
@@ -1,10 +1,52 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
+import {Observable,Subscriber} from 'rxjs';
+import * as XLSX from "xlsx"
 
 @Directive({
   selector: '[appReadexcel]'
 })
 export class ReadexcelDirective {
 
+  excelObservable:Observable<any>;
+
+  @Output() eventEmitter = new EventEmitter();
   constructor() { }
 
+  @HostListener("change",["$event.target"])
+  onChange(target:HTMLInputElement){
+    const file = target.files[0]
+    
+
+    const excelObservable = new Observable((subscriber:Subscriber<any>) => {
+      this.readFile(file,subscriber)
+    });
+    excelObservable.subscribe((d)=>{
+      this.eventEmitter.emit(d);
+    });
+  }
+  readFile(file:File,subscriber:Subscriber<any>){
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file)
+
+
+    fileReader.onload=(e)=>{
+      const bufferArray = e.target.result;
+
+      const wb:XLSX.WorkBook =  XLSX.read(bufferArray,{type:'buffer'});
+      let sheets = wb.Sheets;
+      let sheetMap:Map<string,any> = new Map<string,any>();
+      for (var i = 0; i < wb.SheetNames.length; ++i) {
+        var ws = wb.Sheets[wb.SheetNames[i]];
+        const data = XLSX.utils.sheet_to_json(ws)
+        sheetMap.set(wb.SheetNames[i],data);
+        // subscriber.next(data);
+      }
+        
+      subscriber.next(sheetMap)
+
+
+
+      subscriber.complete();
+    }
+  }
 }
--- src/app/pages/import/import.component.ts
+++ src/app/pages/import/import.component.ts
@@ -7,12 +7,29 @@ import { read, readFile, writeFileXLSX} from "xlsx";
   styleUrls: ['./import.component.scss']
 })
 export class ImportComponent implements OnInit {
-
+  private sheetHash:Map<string,[]> = new Map<string,[]>();
+  private sheetNames:string[] = ["None"];
+  disableButton:boolean = true;
   constructor() { }
 
+  DataFromEventEmitter(event){
+
+    this.sheetHash = event;
+    this.sheetNames = Array.from(this.sheetHash.keys());
+    // console.log(event);
+    let sheet_json = JSON.stringify(this.sheetHash.get(this.sheetNames[4]))
+    // console.log(sheet_json)
+    if (this.sheetNames.length >0){
+      this.disableButton = false;
+    }
+  }
   ngOnInit(): void {
     
 
+  }
+
+  submitClicked(){
+    console.log("that's hot")
   }
 
 }
